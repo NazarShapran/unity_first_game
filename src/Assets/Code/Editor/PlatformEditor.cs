08c0b3d4c6f2ab875e773b559bf9628f29f55193
NazarShapran <nazar.shapran@oa.edu.ua>
2024-12-15 17:52:38 +0200

    feat: visually visible routes

---
+++ src/Assets/Code/Editor/PlatformEditor.cs
@@ -0,0 +1,37 @@
+﻿using Code.Runtime.Gameplay.Logic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Code.Editor
+{
+    [CustomEditor(typeof(Platform))]
+    public class PlatformEditor : UnityEditor.Editor
+    {
+        [DrawGizmo(GizmoType.Selected)]
+        private static void RenderCustomGizmo(Platform platform, GizmoType gizmo)
+        {
+            if (platform == null)
+            {
+                return;
+            }
+            if (platform.PointA == null || 
+                platform.PointB == null || 
+                platform.PointC == null)
+                return;
+
+            Handles.color = Color.red;
+
+            Handles.DrawLine(platform.PointA.position, platform.PointB.position);
+            Handles.DrawLine(platform.PointB.position, platform.PointC.position);
+            Handles.DrawLine(platform.PointC.position, platform.PointA.position);
+
+            Handles.SphereHandleCap(0, platform.PointA.position, Quaternion.identity, 0.2f, EventType.Repaint);
+            Handles.SphereHandleCap(0, platform.PointB.position, Quaternion.identity, 0.2f, EventType.Repaint);
+            Handles.SphereHandleCap(0, platform.PointC.position, Quaternion.identity, 0.2f, EventType.Repaint);
+
+            Handles.Label(platform.PointA.position, "Point A", EditorStyles.boldLabel);
+            Handles.Label(platform.PointB.position, "Point B", EditorStyles.boldLabel);
+            Handles.Label(platform.PointC.position, "Point C", EditorStyles.boldLabel);
+        }
+    }
+}
